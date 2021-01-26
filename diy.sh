4626c7c90f92493a26c1a597fae90a1ae9bf37d1
SunvidWong <45497358+SunvidWong@users.noreply.github.com>
2021-01-27 00:38:58 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,4 +6,4 @@
 # Blog: https://www.sunvid.cn
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/10.0.7.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.5.1/g' package/base-files/files/bin/config_generate
